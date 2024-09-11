cc8b1412264141a1d9eed42a3aea39a5bbc6ea80
EPKlimova <klimovaqa@gmail.com>
2024-09-11 11:15:46 +0300

    Added check file path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,31 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
+
+        // Задание по теме "Циклы"
+        Scanner scanner = new Scanner(System.in);
+        int correctFilesCount = 0; // Счетчик для количества верно указанных путей к файлам
+
+        while (true) {
+            String path = scanner.nextLine();
+            File file = new File(path);
+
+            if (!file.exists()) {
+                System.out.println("Указанный файл не существует.");
+                continue; // Переходим к следующему циклу, не выполняя дальнейшие проверки
+            }
+
+            if (file.isDirectory()) {
+                System.out.println("Указанный путь является путем к папке, а не к файлу.");
+                continue; // Переходим к следующему циклу, не выполняя дальнейшие проверки
+            }
+
+            // Если мы дошли до этого места, значит файл существует и является файлом
+            System.out.println("Путь указан верно.");
+            correctFilesCount++; // Увеличиваем счетчик верно указанных путей
+            System.out.printf("Это файл номер %d%n", correctFilesCount);
+        }
     }
 }
+
+
